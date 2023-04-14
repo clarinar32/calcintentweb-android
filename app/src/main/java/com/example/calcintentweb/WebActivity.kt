82e322f6f67d6fcf5b8f09a063fbebd811439f6d
clarinar32 <clarismukhwana1@gmail.com>
2023-04-14 03:56:40 -0400

    calcintentweb andoid

--- app/src/main/java/com/example/calcintentweb/WebActivity.kt
+++ app/src/main/java/com/example/calcintentweb/WebActivity.kt
@@ -1,11 +1,34 @@
 package com.example.calcintentweb
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+
 
 class WebActivity : AppCompatActivity() {
+    lateinit var mywebView:WebView
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+        mywebView=findViewById(R.id.webView)
+        webview()
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webview(){
+        mywebView.webViewClient= WebViewClient()
+        mywebView.apply{
+            loadUrl("https://www.instagram.com/")
+            settings.javaScriptEnabled=true
+//            settings.safeBrowsingEnabled=true
+        }
+    }
+
+    override fun onBackPressed() {
+        if(mywebView.canGoBack())mywebView.goBack()else
+        super.onBackPressed()
     }
 }
